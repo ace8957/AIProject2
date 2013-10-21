e63120d691c88664a121ac7796c293de40e880a3
Adam Wilford <canadianmoose8957@gmail.com>
2013-10-21 11:33:19 -0400

    added new dict-based code for handling game board.\nStarted to eliminate preexisting values.

--- P2.py
+++ P2.py
@@ -1,19 +1,25 @@
-print("hello world")
 import math
 #TODO: remember to add command-line parsing for the filename
 class SudokuSolver:
-    grid = []
-    frontier = []
-    currentNode = []
-    rows = []
-    columns = []
-    squares = []
-    statefile = ''
+
+    rows = "ABCDEFGHI"
+    columns = "123456789"
+    potential_values = "123456789"
+    indexes = [] #contains the row x colum index used to denote a square
+    grid = {} #contains the potential values of each of the squares
+    
     def __init__(self, filename):    
         self.statefile = filename
+        for c in self.rows:
+            for d in self.columns:
+                self.indexes.append(c+d)
 
+    #parsing the file will also create a dict which has the potential
+    #values for each square included in it
     def parseFile(self):
         file = open(self.statefile, 'r')
+        counter = 0
+        elim = []
         for line in file:
             #take each of the characters and place it into a list
             for char in line:
@@ -22,52 +28,37 @@ class SudokuSolver:
                     continue
                 else:
                     #append the character to the grid list
-                    self.grid.append(char)
-        #I'm wasting a bunch of memory doing this, but I want to make it
-        #easier to code
-        
-    #A function which will update the programmer-friendly data structures
-    #which contain the values in each row, column, and square
-    def updateHelperStructures(self):
-        sideLength = int(math.sqrt(len(self.grid)))
-        squareSideLength = int(math.sqrt(sideLength))
-        l = []
-        count = 0
-        self.columns = [[] for j in range(sideLength)]
-        for i in range(0, len(self.grid)):
-            #squareColumnIndex = count / squareSideLength;
-            #squareRowIndex = sideLength / 
-            l.append(self.grid[i])
-            self.columns[count].append(self.grid[i])
-            count += 1
-            if count == sideLength:
-                self.rows.append(l)
-                l = []
-                count = 0
+                    if char != '.':
+                        self.grid[self.indexes[counter]] = char
+                        elim.append(self.indexes[counter])
+                    else:
+                        self.grid[self.indexes[counter]] = self.potential_values
+                    counter += 1
+        for index in elim:
+            self.remove_used_value(index, self.grid[index])
 
-    #a function to test row parsing and print current values
-    def printRows(self):
-        for row in self.rows:
-            s = ''
-            for entry in row:
-                s += entry
-            print(row)
-        
-    def printGrid(self):
-        sideLenght = int(math.sqrt(len(self.grid)))
-        formatGrid = ''
-        count = 0
-        for entry in self.grid:
-            if count == sideLenght:
-                formatGrid += '\n'
-                count = 0
-            formatGrid += entry
-            count += 1
-        print(formatGrid)
+    #remove a provided character from a string and return the new string
+    def remove_char_from_string(self, s, c):
+        n  = ''
+        if c not in s:
+            return
+        for char in s:
+            if char != c:
+                n += char
+        return n
+
+    #remove a value from the row, column, and square once it is taken
+    def remove_used_value(self, index, value):
+        row = index[0]
+        column = index[1]
+        #remove taken number from the same row and column
+        for key in self.grid.keys():
+            if key[0] == row or key[1] == column:
+                if value in self.grid.get(key) and key != index:
+                    self.grid[key] = self.remove_char_from_string(self.grid.get(key), value)
 
 sd = SudokuSolver('C:\\Users\\Adam\\git\\AIProject2\\state3.txt')
 sd.parseFile()
-sd.updateHelperStructures()
-sd.printGrid()
-print("Printing rows")
-sd.printRows()
+print(sd.grid)
+
+
