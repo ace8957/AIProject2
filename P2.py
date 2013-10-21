4f2c49ea616a204befaa77312eaca99093ee9142
Adam Wilford <canadianmoose8957@gmail.com>
2013-10-21 11:59:49 -0400

    Added a squares structure which contains the indexes contained in each unit.

--- P2.py
+++ P2.py
@@ -2,11 +2,23 @@ import math
 #TODO: remember to add command-line parsing for the filename
 class SudokuSolver:
 
+    MAX_LENGTH = 9
+    
     rows = "ABCDEFGHI"
     columns = "123456789"
     potential_values = "123456789"
-    indexes = [] #contains the row x colum index used to denote a square
-    grid = {} #contains the potential values of each of the squares
+    indexes = [] #contains the row x colum index used to denote a tile
+    grid = {} #contains the potential values of each of the tiles
+    #squares = [[] for x in range(0,MAX_LENGTH)]
+    squares = [['A1','A2','A3','B1','B2','B3','C1','C2','C3'],
+               ['D1','D2','D3','E1','E2','E3','F1','F2','F3'],
+               ['G1','G2','G3','H1', 'H2','H3','I1','I2','I3'],
+               ['A4','A5','A6','B4','B5','B6','C4','C5','C6'],
+               ['D4','D5','D6','E4','E5','E6','F4','F5','F6'],
+               ['G4','G5','G6','H4', 'H5','H6','I4','I5','I6'],
+               ['A7','A8','A9','B7','B8','B9','C7','C8','C9'],
+               ['D7','D8','D9','E7','E8','E9','F7','F8','F9'],
+               ['G7','G8','G9','H7', 'H8','H9','I7','I8','I9']]
     
     def __init__(self, filename):    
         self.statefile = filename
@@ -59,6 +71,6 @@ class SudokuSolver:
 
 sd = SudokuSolver('C:\\Users\\Adam\\git\\AIProject2\\state3.txt')
 sd.parseFile()
-print(sd.grid)
+print(sd.squares)
 
 
